7f6e3557fbb8247d2f2596d02954fcb42b9a9f07
Abdul basit <dev17@zamsol.com>
2019-12-05 10:36:29 +0500

    Update another_testing.php
    Adding Asid element to page

--- another_testing.php
+++ another_testing.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>dfdfdfdfffdf</title>
+</head>
+<body>
+	<aside>
+		<ul>
+			<li>Home</li>
+			<li>About</li>
+			<li>Settings</li>
+		</ul>	
+	</aside>
+</body>
+</html>
